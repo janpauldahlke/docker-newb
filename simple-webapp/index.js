bc8743fe2c591216cb47c3b086de4e88d8fb3554
HagbardCeline <janpauldahlke@gmail.com>
2022-11-21 10:55:48 +0100

    using the build cache for npm install in two seperate COPY commands

--- simple-webapp/index.js
+++ simple-webapp/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`Hello there stranger!`);
+  res.send(`Hello there stranger! weird times to life in.`);
 })
 
 // -- PORT
