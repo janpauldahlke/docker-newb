ee8962537856574990c744c34c79e92d8abf36b1
HagbardCeline <janpauldahlke@gmail.com>
2022-11-21 12:29:07 +0100

    connecting to containers to a visit counter+

--- visitor-counter/index.js
+++ visitor-counter/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const redis = require('redis');
+const _VISITS =  'visits'
 
 const app = express();
-const client = redis.createClient();
-
-
+const client = redis.createClient({
+  //host: 'https://myAdress.com' // not in docker
+  host: 'redis-server', // the name needs to be similar to the name in the docker-compose.yml naming
+  port: 6379 // default redis port
+});
 //init number of visits to 0
-client.set('visits', 0);
+client.set(_VISITS, 0);
 
 app.get('/', (req, res) => {
-  client.get('client', (err, visits) => {
+  client.get(_VISITS, (err, visits) => {
     if (err) console.log(err);
     res.send(`Hello Vistor, thou are guest ${visits}`);
-    client.set('visits', parseInt(visits) + 1);
+    client.set(_VISITS, parseInt(visits) + 1);
   })
 });
 
