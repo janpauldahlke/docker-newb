aa6e6ac01be53ca70afe6b54c9295b7b9cea48a0
HagbardCeline <janpauldahlke@gmail.com>
2022-11-21 09:50:41 +0100

    simple webapp run on -p mapped ports and is visible to the outside world aka localhost

---
+++ simple-webapp/index.js
@@ -0,0 +1,12 @@
+// -- SimpleWebapp
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`Hello there stranger!`);
+})
+
+// -- PORT
+app.listen(8080, () => {
+  console.log('Listening on :8080')
+})
